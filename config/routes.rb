c8fa5863bdd29a28098653f366db8c3ba999013c
Cory Rodriguez <coreyreylp@gmail.com>
2015-12-20 20:05:34 -0600

    Added user controller with sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'resume'    => 'static_pages#resume'
   get 'portfolio' => 'static_pages#portfolio'
+  get 'signup'    =>'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
