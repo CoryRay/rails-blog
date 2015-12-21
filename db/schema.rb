0d4b74015b624db97a224786267e1768bf53f555
Cory Rodriguez <coreyreylp@gmail.com>
2015-12-21 16:28:25 -0600

    added user tests and validation for email and password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221020548) do
+ActiveRecord::Schema.define(version: 20151221220841) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -36,5 +36,7 @@ ActiveRecord::Schema.define(version: 20151221020548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
