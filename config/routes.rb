089ade2752e2f3308e61e4b1190c2d8062caa3d5
Cory Rodriguez <coreyreylp@gmail.com>
2015-12-24 09:53:42 -0600

    users can log in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
 
   get 'resume'    => 'static_pages#resume'
   get 'portfolio' => 'static_pages#portfolio'
-  get 'signup'    =>'users#new'
+
+  get 'signup' =>'users#new'
+
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
